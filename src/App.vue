e77cefa7faa65a0bcaa0e63b46a0fa9cc4ac5618
Erik Hanchett <erik.hanchett@gmail.com>
2020-03-08 19:30:29 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,75 @@
 <template>
+  <div id="portal-target"></div>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <portal target="#portal-target">
+      <Popup ref="popup" :title="'Popup test2'" :sub="'Erik Hanchett '">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda dolor
+        saepe perspiciatis, facere rem iste doloribus ut illo fugit nam delectus
+        aliquid totam voluptates neque deleniti sequi animi impedit adipisci.
+      </Popup>
+    </portal>
+    <button class="btn" @click="$refs.popup.show = !$refs.popup.show">
+      Open Modal
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Popup from "./components/Popup.vue";
 
 export default {
   name: "App",
+  data() {
+    return {
+      show: false
+    };
+  },
   components: {
-    HelloWorld
+    Popup
   }
 };
 </script>
 
-<style>
+<style lang="scss">
+/* Demo Styles */
+
+html,
+body {
+  height: 100%;
+}
+
+body {
+  font: 600 18px/1.5 -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen-Sans, Ubuntu, Cantarell, "Helvetica Neue", sans-serif;
+  background-image: linear-gradient(to right, #7f53ac 0, #657ced 100%);
+  color: black;
+}
+
+a {
+  color: inherit;
+}
+
+.container {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+small {
+  color: #aaa;
+}
+
+.btn {
+  font-size: 36px;
+  background-color: #fff;
+  padding: 1em 1.5em;
+  border-radius: 3px;
+  text-decoration: none;
+  i {
+    padding-right: 0.3em;
+  }
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
